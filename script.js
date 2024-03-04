50db9d24e7bcbbe2e88350438cce7fc6cafdb060
deepak <deepakrajesh567@gmail.com>
2024-03-04 20:30:02 +0530

    displayed chat message to the chatbox from the text area

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 const chatInput = document.querySelector(".chat-input textarea");
 const sendChatButton = document.querySelector(".chat-input span");
+const chatbox = document.querySelector(".chatbox")
 
 let userMessage;
 
-const handleChat = ()=>{
+const createChatLi = (message,className)=>{
+    const chatLi = document.createElement("li");
+    chatLi.classList.add("chat",className);
+    let chatContent = className === "outgoing"? `<p>${message}</p>`:` <span class="material-symbols-outlined">
+    smart_toy
+</span>
+<p>${message}</p>`;
+chatLi.innerHTML = chatContent;
+return chatLi;
+}
+
+
+const handleChat = () => {
     userMessage = chatInput.value.trim();
-    console.log(userMessage);
+    if(!userMessage)return;
+    chatbox.appendChild(createChatLi(userMessage,"outgoing"));
 }
 sendChatButton.addEventListener("click",handleChat);
