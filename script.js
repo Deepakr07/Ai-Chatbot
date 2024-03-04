af5a4443ba7d78d1914b136f1095b13a8256ca6c
deepak <deepakrajesh567@gmail.com>
2024-03-04 19:52:36 +0530

    displayed message from the textarea to the console

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ const chatInput = document.querySelector(".chat-input textarea");
 const sendChatButton = document.querySelector(".chat-input span");
 
 let userMessage;
+
+const handleChat = ()=>{
+    userMessage = chatInput.value.trim();
+    console.log(userMessage);
+}
+sendChatButton.addEventListener("click",handleChat);
